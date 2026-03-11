76ebb98a6d064cff40770c6c806f6ca1e7f07f1b
omale joseph <mrjay0505@gmail.com>
2026-03-10 20:01:57 -0700

    AdminProductSlice fetch correction

--- frontend/src/redux/slices/adminProductSlice.js
+++ frontend/src/redux/slices/adminProductSlice.js
@@ -8,7 +8,7 @@ const USER_TOKEN = `Bearer ${localStorage.getItem("userToken")}`;
 export const fetchAdminProducts = createAsyncThunk(
   "adminProducts/fetchProducts",
   async () => {
-    const response = await axios.get(`${API_URL}/api/admin/products`, {
+    const response = await axios.get(`${API_URL}/api/products`, {
       headers: {
         Authorization: USER_TOKEN,
       },
